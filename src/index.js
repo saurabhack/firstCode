2aebbaece7ec4d07f1c259af63f3b683f5792098
saurabhack <kamanesaurabh68@gmail.com>
2024-07-17 13:25:54 +0530

    mongodb atlas connection

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+// import {app} from "./app.js"
+dotenv.config({path:'./.env'})
+
+connectDB()
+
+
+
+
+// (async ()=>{
+//     try {
+//         mongoose.connect()
+//     }catch(error){
+//         console.error("ERROR",error)
+//         throw err
+//     }
+// })()
