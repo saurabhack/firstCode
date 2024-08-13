64dbecee03013b656e6ad651fe74859e1d1e1773
saurabhack <kamanesaurabh68@gmail.com>
2024-08-13 11:53:23 +0530

    updated code of database connection using node and express js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 // import {app} from "./app.js"
 dotenv.config({path:'./.env'})
